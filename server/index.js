58a0a37f82dac2790464f8a47a1dae68da63b912
Adam Kendis <atkendis@gmail.com>
2018-12-01 18:01:06 -0800

    Fixed server.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const config = require('../knexfile.js')['development'];
+const knex = require('knex')(config);
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, '/../dist')));
+
+app.get('/items', (req, res) => {
+  knex('items')
+    .then( items => {
+      console.log(items);
+      res.send(items);
+    });
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
